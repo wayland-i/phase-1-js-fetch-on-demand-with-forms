850796ccc2603d880e27fd58ea8405917b01fb6b
Ian Iversen <ianiversen1@gmail.com>
2022-07-15 20:02:50 -0400

    commit tree 53a67188881b8d5813d83df6d7a9c891278da182

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const init = () => {
   
+
+
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const input = e.target.children[1].value
+        // console.log(input)
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(movies => {
+            const title = document.querySelector('#movieDetails').children[0]
+            const summary = document.querySelector('#movieDetails').children[1]
+
+            
+            title.innerText = movies.title
+            summary.innerText = movies.summary
+
+            // const summary
+        })
+
+        
+
+    })
+
+
+
+
+
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
